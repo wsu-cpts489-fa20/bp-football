f6d5e92940823d64a55609a769e3fb1d99eb03ad
Brandon Le <brandon.le@wsu.edu>
2020-11-30 15:38:18 -0800

    test

--- client/src/AppMode.js
+++ client/src/AppMode.js
@@ -1,13 +1,13 @@
 /* AppMode: The enumerated type for AppMode. */
 
 const AppMode = {
-    LOGIN: "LoginMode",
-    FEED: "FeedMode",
-    ROUNDS: "RoundsMode",
-    ROUNDS_LOGROUND: "RoundsMode-LogRound",
-    ROUNDS_EDITROUND: "RoundsMode-EditRound",
-    COURSES: "CoursesMode",
-    DRAFT: "DraftMode"
+  LOGIN: "LoginMode",
+  Team: "TeamMode",
+  ROUNDS: "RoundsMode",
+  ROUNDS_LOGROUND: "RoundsMode-LogRound",
+  ROUNDS_EDITROUND: "RoundsMode-EditRound",
+  COURSES: "CoursesMode",
+  DRAFT: "DraftMode",
 };
 
 Object.freeze(AppMode); //This ensures that the object is immutable.
