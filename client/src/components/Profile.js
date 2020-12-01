60420ce786922bd4013b53bdb383a5473416e818
Brandon Le <brandon.le@wsu.edu>
2020-11-30 21:02:49 -0800

    added starter profile page file

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,19 @@
+// ToDo:
 // display user information such as:
 // achievements
 // team name
 // display name
 // etc.
+import React from "react";
+
+class ProfilePage extends React.Component {
+  render() {
+    return (
+      <div className="padded-page">
+        <center></center>
+      </div>
+    );
+  }
+}
+
+export default ProfilePage;
