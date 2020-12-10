cf1069658c48c18184fd7513f265ecccf8c6e04b
drewmclaurin <38591600+drewmclaurin@users.noreply.github.com>
2020-12-09 20:23:44 -0800

    DraftPage UI ready for backend integration

--- client/src/components/DraftPage.js
+++ client/src/components/DraftPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CSVReader from "react-csv-reader";
+
 class DraftPage extends React.Component {
   constructor(props) {
     super(props);
@@ -8,7 +8,16 @@ class DraftPage extends React.Component {
       QBList: [],
       RBList: [],
       WRList: [],
+      TEList: [],
+      KList: [],
+      DefList: [],
 
+      qb: "",
+      rb: "",
+      wr: "",
+      te: "",
+      k: "",
+      def: "",
     };
   }
 
@@ -36,12 +45,12 @@ class DraftPage extends React.Component {
   };
 
   createLists = () => {
-    console.log(this.props.playerData.players);
+    // console.log(this.props.playerData.players);
     if(this.props.playerData != 0 && this.state.QBList.length == 0){
       for(let i = 0; i < this.props.playerData.players.length; i++){
-        console.log(this.props.playerData.players[i]);
+        // console.log(this.props.playerData.players[i]);
    
-        console.log(this.props.playerData.players[i].player.fullName);
+        // console.log(this.props.playerData.players[i].player.fullName);
         if(this.props.playerData.players[i].player.defaultPositionId == 1){
           this.state.QBList.push(this.props.playerData.players[i].player.fullName);
         }
@@ -51,34 +60,95 @@ class DraftPage extends React.Component {
         else if(this.props.playerData.players[i].player.defaultPositionId == 3){
           this.state.WRList.push(this.props.playerData.players[i].player.fullName);
         }
+        else if(this.props.playerData.players[i].player.defaultPositionId == 4){
+          this.state.TEList.push(this.props.playerData.players[i].player.fullName);
+        }
+        else if(this.props.playerData.players[i].player.defaultPositionId == 5){
+          this.state.KList.push(this.props.playerData.players[i].player.fullName);
+        }
+        else if(this.props.playerData.players[i].player.defaultPositionId == 16){
+          this.state.DefList.push(this.props.playerData.players[i].player.fullName);
+        }
+
+        
 
         // this.state.NFLPlayerList.push(this.props.playerData.players[i].player.fullName);
       }
     }
   }
 
+  handleSubmit = () => {
+    //send team info to backend
+
+
+  }
+
   render() {
-    this.props.getCurrentData();
-    this.createLists();
-    var MakeItem = function(X) {
-        return <option>{X}</option>;
-    };    return (
+    if (this.state.QBList.length == 0){
+      this.props.getCurrentData();
+      this.createLists();
+    }
+      var MakeItem = function(X) {
+          return <option>{X}</option>;
+      };   
+    
+    return (
     <div className="padded-page">
       <h1>Draft Team</h1>
       {this.props.playerData != 0 ?
       <div>
-        QB: 
-        <select style={{width: `${150}px`}}>
-          {(this.state.QBList).map(MakeItem)}
-        </select>
-        RB: 
-        <select style={{width: `${150}px`}}>
-          {(this.state.RBList).map(MakeItem)}
-        </select>
-        WR: 
-        <select style={{width: `${150}px`}}>
-          {(this.state.WRList).map(MakeItem)}
-        </select>
+        <center>
+          QB: 
+          <select style={{width: `${150}px`}} value={this.state.qb}
+                  onChange={(value) => { this.setState({ qb: value.target.value }); }}>
+            {(this.state.QBList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          RB: 
+          <select style={{width: `${150}px`}} value={this.state.rb}
+                  onChange={(value) => { this.setState({ rb: value.target.value }); }}>          
+            {(this.state.RBList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          WR: 
+          <select style={{width: `${150}px`}} value={this.state.wr}
+                  onChange={(value) => { this.setState({ wr: value.target.value }); }}>
+            {(this.state.WRList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          TE: 
+          <select style={{width: `${150}px`}} value={this.state.te}
+                  onChange={(value) => { this.setState({ te: value.target.value }); }}>
+            {(this.state.TEList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          K: 
+          <select style={{width: `${150}px`}} value={this.state.k}
+                  onChange={(value) => { this.setState({ k: value.target.value }); }}>
+            {(this.state.KList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          Def: 
+          <select style={{width: `${150}px`}} value={this.state.def}
+                  onChange={(value) => { this.setState({ def: value.target.value }); }}>
+            {(this.state.DefList).map(MakeItem)}
+          </select>
+          <br/>
+          <br/>
+          <button
+              type="submit"
+              className="btn-color-theme btn btn-primary btn-block"
+              onClick={this.handleSubmit()}
+            >
+              <span id="draft-btn-icon" className="submit-draft" />
+              &nbsp;Submit Team Draft
+          </button>
+        </center>
       </div>
       : null }
     </div>
